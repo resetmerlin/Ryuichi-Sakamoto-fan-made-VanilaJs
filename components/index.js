40d158e90bda7cff11fc5e9e41acbde135b70e65
resetmerlin <resetmerlin@gmail.com>
2023-12-29 17:03:12 +0900

    feat: Add phone custom component

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import { Description } from './description/Description.js';
 import { Donut } from './donut/Donut.js';
 import { Header } from './header/Header.js';
+import { Phone } from './phone/Phone.js';
 import { Song } from './song/Song.js';
 
 customElements.define('custom-header', Header);
 customElements.define('custom-donut', Donut);
 customElements.define('custom-description', Description);
 customElements.define('custom-song', Song);
+customElements.define('custom-phone', Phone);
--- components/phone/Phone.js
+++ components/phone/Phone.js
@@ -6,7 +6,7 @@ export class Phone extends HTMLElement {
 
   connectedCallback() {
     this.shadowRoot.innerHTML = `
-      <style>${headerStyle}</style>
+      <style>${phoneStyle}</style>
       <div class="box">
       <div class="inner">
         <div class="icons">
@@ -92,50 +92,100 @@ export class Phone extends HTMLElement {
   }
 }
 
-const headerStyle = `
-  .header {
-      width: 100%;
-      position: relative;
-      top: 0;
-      height: 9vh;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      background-color: #000;
-    }
-    .header__logo {
-      display: flex;
-      align-items: center;
-      font-size: 2rem;
-      font-weight: 600;
-      cursor: pointer;
-      margin-left: 10rem;
-    }
-    .header__logo a {
-      text-decoration: none;
-      color: #fff;
-      transition: all 0.5s ease;
-    }
-  
-    .header__navigation {
-      display: flex;
-      list-style: none;
-      padding: 1.5rem 0;
-    }
-    .header__navigation a {
-      text-decoration: none;
-      font-size: 1.5rem;
-      color: #fff;
-    }
-    .header__navigation li {
-      margin: 0 2rem;
-      display: inline-block;
-      transition: all 0.2s ease;
-      cursor: pointer;
-    }
-    .header li:hover {
-      transform: translateY(-1rem) scale(1.03);
-      border-bottom: 1px solid #fff;
-    }
-  
+const phoneStyle = `
+
+.box {
+  position: absolute;
+  left: 68%;
+  top: 15%;
+  width: 57rem;
+  height: 110rem;
+  background-color: #666666;
+  border-radius: 10rem;
+  box-shadow: 0px 5px 10rem 5px rgba(255, 255, 255, 0.7);
+  z-index: 10;
+}
+.box::before {
+  content: '';
+  position: absolute;
+  inset: 0.1rem;
+  background-color: #6b6a6a;
+  border-radius: 10rem;
+}
+.box .inner {
+  height: 100%;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.9) 0px 18px 36px -18px inset,
+    rgba(0, 0, 0, 0.5) 0px -50px 36px -28px inset;
+  position: absolute;
+  inset: 0.2rem;
+  background-color: white;
+  background-size: cover;
+  background-position: center;
+  border-radius: 10rem;
+  height: auto;
+  border: 2rem solid #000;
+  display: flex;
+  justify-content: center;
+  opacity: 1;
+  transition: 0.5s;
+}
+.box .inner .icons {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  justify-content: center;
+  top: 1.8%;
+  right: 12%;
+  font-size: 3.8rem;
+}
+.box .inner .icons .bxs-battery {
+  margin-top: 1rem;
+  font-size: 3.5rem;
+}
+.box .inner .icons .bx-signal-4 {
+  margin-right: -1rem;
+}
+.box .inner .island_popup {
+  position: absolute;
+  top: 2rem;
+  width: 16rem;
+  height: 5rem;
+  background-color: #000;
+  border-radius: 10rem;
+  transition: 0.5s ease-in-out;
+}
+.box:hover .inner {
+  opacity: 1;
+}
+.box .btn {
+  position: absolute;
+  top: 17rem;
+  left: -0.7rem;
+  width: 0.5rem;
+  height: 6rem;
+  border-top-left-radius: 2rem;
+  border-bottom-left-radius: 2rem;
+  background: radial-gradient(rgba(160, 160, 160, 0.726), #5c5c5c, #252525);
+  z-index: 10;
+}
+.box .btn.btn2 {
+  top: 30rem;
+  height: 8rem;
+}
+.box .btn.btn3 {
+  top: 41rem;
+  height: 8rem;
+}
+.box .rightSideBtn {
+  position: absolute;
+  top: 25rem;
+  right: -0.3rem;
+  width: 0.5rem;
+  height: 12rem;
+  border-top-right-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+  background: radial-gradient(rgba(160, 160, 160, 0.726), #5c5c5c, #252525);
+  z-index: 10;
+}
   `;
